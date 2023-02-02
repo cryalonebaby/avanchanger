8b3cdb38bf94c01b25e34108ffdf613c6ffdef00
cryalonebaby <cryalonebabyyy@gmail.com>
2023-02-02 05:44:04 +0200

    added ratio and personal percent for coins

--- models/Wallet.js
+++ models/Wallet.js
@@ -4,6 +4,7 @@ const schema = new Schema({
     symbol: { type: String, required: true },
     address: { type: String, required: true },
     qr: { type: String, required: false },
+    percent: { type: Number, required: false },
 })
 
 module.exports = model('Wallet', schema)
