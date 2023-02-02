b26d85f88a19469ec131dd4ec9131bf1e1949cdb
cryalonebaby <cryalonebabyyy@gmail.com>
2023-02-02 19:34:00 +0200

    added tg bot and favicon build #2

--- client/src/components/Telegram/Telegram.js
+++ client/src/components/Telegram/Telegram.js
@@ -8,7 +8,7 @@ const Telegram = () => {
       <FaTelegramPlane color='white' size={32} className={s.icon} />
       <h5>Попробуй нашего Telegram бота!</h5>
       Встречайте нашего забавного Telegram бота-менялу из лихих 90-х. Бот Валера, поможет вам еще быстрее совершать обмены.
-      <a href="https://t.me/avanchange_eu" className={s.btn} target="_blank" rel='noreferrer'>
+      <a href="https://t.me/avanchange_bot" className={s.btn} target="_blank" rel='noreferrer'>
         <FaTelegramPlane color='#0facf3' size={20} className={s.btnIcon} />
         Попробовать
       </a>
