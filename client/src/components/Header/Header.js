9f8a038b3f8b2ca7a8f9f698f44f11815a61f92f
cryalonebaby <cryalonebabyyy@gmail.com>
2023-02-21 07:15:54 +0200

    change header email

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,17 +2,17 @@ import React from 'react'
 import s from './style.module.css'
 import cn from 'classnames'
 import { useState, useContext } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import ModalLogin from '../../Modals/ModalLogin'
 import { AuthContext } from '../../context/AuthContext';
 import ModalRegister from '../../Modals/ModalRegister';
-import {FaTelegramPlane} from 'react-icons/fa'
-import {FiMail} from 'react-icons/fi'
-import {FiUser} from 'react-icons/fi'
-import {ImSwitch} from 'react-icons/im'
+import { FaTelegramPlane } from 'react-icons/fa'
+import { FiMail } from 'react-icons/fi'
+import { FiUser } from 'react-icons/fi'
+import { ImSwitch } from 'react-icons/im'
 import Navbar from '../Navbar/Navbar';
 
-const Header = ({isAuth}) => {
+const Header = ({ isAuth }) => {
     const auth = useContext(AuthContext)
     const logOut = () => {
         auth.logout()
@@ -31,12 +31,12 @@ const Header = ({isAuth}) => {
                     <div className={s.left}>
                         <Link to={'/ru/'} target={'_blank'} rel={'noreferrer'} className={cn(s.eas, s.lang)}>RU</Link>
                         <a href="https://t.me/avanchange_bot" rel="noreferrer" target="_blank" className={cn(s.eas, s.headerTelegram)}>
-                            <FaTelegramPlane color='#0facf3' size={12}/>
+                            <FaTelegramPlane color='#0facf3' size={12} />
                             <span> Telegram Бот v1.3</span>
                         </a>
                         <Link to={'/support'} target={'_blank'} rel={'noreferrer'} className={cn(s.eas, s.headerEmail)}>
-                            <FiMail size={12} color='#3dd94b'/>
-                            <span> info@avanchange.com</span>
+                            <FiMail size={12} color='#3dd94b' />
+                            <span> avanchange.eu@gmail.com</span>
                         </Link>
                         <span className={s.worktime}>
                             <span>
@@ -48,26 +48,26 @@ const Header = ({isAuth}) => {
                         <span className={cn(s.flaticonUser, s.ico4)}></span>
                         {
                             isAuth ?
-                            <div className={s.signWrapper}>
-                                <FiUser size={14} color='#ff7d00' fill='#ff7d00'/>
-                                <Link to={'/account'} className={cn(s.eas, s.url)}> Мой аккаунт </Link>
-                                <Link to={'/'} className={s.logout} onClick={logOut}>
-                                    <ImSwitch size={12} color='#ff7d00'/>
-                                </Link>
-                            </div> :
-                            <div className={s.signWrapper}>
-                                <FiUser size={14} color='#ff7d00' fill='#ff7d00'/>
-                                <span className={s.eas} onClick={() => setLoginModalActive(true)}> Вход </span>
-                                /
-                                <span className={s.eas} onClick={() => setRegisterModalActive(true)}> Регистрация</span>
-                            </div>
+                                <div className={s.signWrapper}>
+                                    <FiUser size={14} color='#ff7d00' fill='#ff7d00' />
+                                    <Link to={'/account'} className={cn(s.eas, s.url)}> Мой аккаунт </Link>
+                                    <Link to={'/'} className={s.logout} onClick={logOut}>
+                                        <ImSwitch size={12} color='#ff7d00' />
+                                    </Link>
+                                </div> :
+                                <div className={s.signWrapper}>
+                                    <FiUser size={14} color='#ff7d00' fill='#ff7d00' />
+                                    <span className={s.eas} onClick={() => setLoginModalActive(true)}> Вход </span>
+                                    /
+                                    <span className={s.eas} onClick={() => setRegisterModalActive(true)}> Регистрация</span>
+                                </div>
 
                         }
                     </div>
                 </div>
             </div>
-            <Navbar/>
-            <ModalLogin 
+            <Navbar />
+            <ModalLogin
                 loginActive={loginModalActive}
                 setLoginActive={setLoginModalActive}
                 switchModals={switchModals}
